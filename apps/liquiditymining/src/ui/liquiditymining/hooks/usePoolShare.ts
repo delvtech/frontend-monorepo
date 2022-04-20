5c15bffc57165d8c91af7bee7555a837384e0e1b
Duke <hnguyen1179@berkeley.edu>
2022-04-20 15:14:25 -0700

    Add documentation

--- apps/liquiditymining/src/ui/liquiditymining/hooks/usePoolShare.ts
+++ apps/liquiditymining/src/ui/liquiditymining/hooks/usePoolShare.ts
@@ -2,6 +2,13 @@ import { useTotalLiquidity } from "@elementfi/core/pools/hooks/useTotalLiquidity
 import { PoolInfo } from "@elementfi/core/pools/PoolInfo";
 import { useUserInfo } from "src/ui/liquiditymining/hooks/useUserInfo";
 
+/**
+ * Function to calculate the percent share of a user's deposit in a specified pool
+ *
+ * @param poolInfo Pool
+ * @param account User address
+ * @returns A string denoting a user's percentage in the specified pool
+ */
 export function usePoolShare(
   poolInfo: PoolInfo,
   account: string | null | undefined,
