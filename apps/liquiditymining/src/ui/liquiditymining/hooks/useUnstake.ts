3722857f542f866522a76aa7e77a08a71786580d
Ryan Goree <goree.ryan@gmail.com>
2022-04-22 19:45:56 -0500

    Invalidate cache after LM transactions

--- apps/liquiditymining/src/ui/liquiditymining/hooks/useUnstake.ts
+++ apps/liquiditymining/src/ui/liquiditymining/hooks/useUnstake.ts
@@ -1,11 +1,13 @@
 import { UseMutationResult } from "react-query";
 import { ContractReceipt, Signer } from "ethers";
 import {
+  matchSmartContractReadCallQuery,
   useSmartContractTransaction,
   UseSmartContractTransactionOptions,
 } from "@elementfi/react-query-typechain";
 import { MCMod } from "@elementfi/peripherals";
 import { masterChef } from "src/elf/liquiditymining/masterChef";
+import { queryClient } from "src/elf/queryClient";
 
 export function useUnstake(
   signer: Signer | undefined,
@@ -15,5 +17,20 @@ export function useUnstake(
   unknown,
   Parameters<MCMod["withdraw"]>
 > {
-  return useSmartContractTransaction(masterChef, "withdraw", signer, options);
+  return useSmartContractTransaction(masterChef, "withdraw", signer, {
+    ...options,
+    onTransactionMined: (...args) => {
+      const [poolId, _, account] = args[1];
+      queryClient.invalidateQueries({
+        predicate: (query) =>
+          matchSmartContractReadCallQuery(
+            query,
+            masterChef.address,
+            "userInfo",
+            [poolId, account as string],
+          ),
+      });
+      options?.onTransactionMined?.(...args);
+    },
+  });
 }
--- apps/liquiditymining/src/ui/liquiditymining/hooks/useUnstakeAndClaim.ts
+++ apps/liquiditymining/src/ui/liquiditymining/hooks/useUnstakeAndClaim.ts
@@ -1,11 +1,13 @@
 import { UseMutationResult } from "react-query";
 import { ContractReceipt, Signer } from "ethers";
 import {
+  matchSmartContractReadCallQuery,
   useSmartContractTransaction,
   UseSmartContractTransactionOptions,
 } from "@elementfi/react-query-typechain";
 import { MCMod } from "@elementfi/peripherals";
 import { masterChef } from "src/elf/liquiditymining/masterChef";
+import { queryClient } from "src/elf/queryClient";
 
 export function useUnstakeAndClaim(
   signer: Signer | undefined,
@@ -15,10 +17,20 @@ export function useUnstakeAndClaim(
   unknown,
   Parameters<MCMod["withdrawAndHarvest"]>
 > {
-  return useSmartContractTransaction(
-    masterChef,
-    "withdrawAndHarvest",
-    signer,
-    options,
-  );
+  return useSmartContractTransaction(masterChef, "withdrawAndHarvest", signer, {
+    ...options,
+    onTransactionMined: (...args) => {
+      const [poolId, _, account] = args[1];
+      queryClient.invalidateQueries({
+        predicate: (query) =>
+          matchSmartContractReadCallQuery(
+            query,
+            masterChef.address,
+            "userInfo",
+            [poolId, account as string],
+          ),
+      });
+      options?.onTransactionMined?.(...args);
+    },
+  });
 }
