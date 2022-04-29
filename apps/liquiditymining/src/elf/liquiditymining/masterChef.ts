bdd24dad14b80b11969663445fbf0ec7acb2d843
Danny Delott <dannydelott@gmail.com>
2022-04-28 18:12:34 -0700

    Show total fiat staked in pool

--- apps/liquiditymining/src/elf/liquiditymining/masterChef.ts
+++ apps/liquiditymining/src/elf/liquiditymining/masterChef.ts
@@ -4,6 +4,7 @@ import { MCMod__factory } from "@elementfi/peripherals/typechain";
 // TODO: Make a masterchef mainnet contract instance
 export const MASTER_CHEF_GOERLI_ADDRESS =
   "0x97CBcFA11318c405F0b9804ce0c7574602c4F762";
+
 export const masterChef = MCMod__factory.connect(
   MASTER_CHEF_GOERLI_ADDRESS,
   defaultProvider,
--- apps/liquiditymining/src/ui/liquiditymining/hooks/useTotalFiatStaked.ts
+++ apps/liquiditymining/src/ui/liquiditymining/hooks/useTotalFiatStaked.ts
@@ -2,6 +2,8 @@ import { PoolInfo } from "@elementfi/core/pools/PoolInfo";
 import { useLPTokenPrice } from "@elementfi/core/pools/hooks/useLPTokenPrice";
 import { useUserInfo } from "src/ui/liquiditymining/hooks/useUserInfo";
 import { eligibleGoerliPoolContracts } from "src/elf/liquiditymining/eligiblepools";
+import { useLPTokenBalance } from "src/ui/liquiditymining/hooks/useLPTokenBalance";
+import { MASTER_CHEF_GOERLI_ADDRESS } from "src/elf/liquiditymining/masterChef";
 
 /**
  * Calculate a user's total fiat value staked in a specified pool
@@ -10,7 +12,7 @@ import { eligibleGoerliPoolContracts } from "src/elf/liquiditymining/eligiblepoo
  * @param account userAddress
  * @returns Returns total fiat value staked in a string format: i.e., "3.50"
  */
-export function useTotalFiatStaked(
+export function useTotalFiatStakedForUser(
   poolInfo: PoolInfo,
   account: string | null | undefined,
 ): string {
@@ -23,4 +25,16 @@ export function useTotalFiatStaked(
   const depositedBalance = userInfo?.amount || "0.0";
 
   return (+lpTokenPrice * +depositedBalance).toFixed(2);
+}
+
+export function useTotalFiatStaked(poolInfo: PoolInfo): string {
+  const poolContract = eligibleGoerliPoolContracts[poolInfo.address];
+  const lpTokenPrice = useLPTokenPrice(poolInfo, poolContract);
+
+  const { data: lpBalance } = useLPTokenBalance(
+    poolContract,
+    MASTER_CHEF_GOERLI_ADDRESS,
+  );
+
+  return (+lpTokenPrice * +(lpBalance || 0)).toFixed(2);
 }
