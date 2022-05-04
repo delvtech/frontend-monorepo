89c291155d3e1d104a5a100ebffb116c208945d3
Ryan Goree <goree.ryan@gmail.com>
2022-05-04 15:31:50 -0500

    Account for NaN in Elfi component

--- apps/liquiditymining/src/ui/liquiditymining/Elfi.tsx
+++ apps/liquiditymining/src/ui/liquiditymining/Elfi.tsx
@@ -15,7 +15,7 @@ export const Elfi = ({ className, amount = 0 }: ElfiProps): ReactElement => {
   return (
     <span className={classNames("inline-flex items-center gap-1", className)}>
       <ElementIconCircle inline size={IconSize.SMALL} />
-      <span>{commify((+amount).toFixed(2))}</span>
+      <span>{commify((+amount || 0).toFixed(2))}</span>
     </span>
   );
 };
