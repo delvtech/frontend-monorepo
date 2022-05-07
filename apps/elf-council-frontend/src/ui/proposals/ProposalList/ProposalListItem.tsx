e099ccb3d859bff8259565f003f879e55a3ebe39
Matthew Brown <mattfdbrown@gmail.com>
2022-05-07 14:33:40 -0500

    move component to it's own file

--- apps/elf-council-frontend/src/ui/proposals/ProposalList/ProposalListItem.tsx
+++ apps/elf-council-frontend/src/ui/proposals/ProposalList/ProposalListItem.tsx
@@ -1,27 +1,18 @@
 import React, { ReactElement, useCallback } from "react";
 
 import { Signer } from "@ethersproject/abstract-signer";
-import {
-  ThumbDownIcon,
-  ThumbUpIcon,
-  XCircleIcon,
-} from "@heroicons/react/solid";
 import classNames from "classnames";
 import { Proposal } from "@elementfi/elf-council-proposals";
-import { formatEther } from "ethers/lib/utils";
 import { t } from "ttag";
 
 import { formatAbbreviatedDate } from "src/base/dates";
 import { MS_PER_S, SECONDS_PER_BLOCK } from "src/base/time";
 import Card from "src/ui/base/Card/Card";
 import CardHeader from "src/ui/base/Card/CardHeader";
-import Tooltip from "src/ui/base/Tooltip/Tooltip";
 import { ProposalStatusIcon } from "src/ui/proposals/ProposalList/ProposalStatusIcon";
 import { useSnapshotProposals } from "src/ui/proposals/useSnapshotProposals";
-import { Ballot } from "src/ui/voting/Ballot";
-import { useBallot } from "src/ui/voting/useBallot";
 import { useLatestBlockNumber } from "src/ui/ethereum/useLatestBlockNumber";
-import { useGSCBallot } from "src/ui/voting/useGSCBallot";
+import { BallotIcon } from "./BallotIcon";
 
 interface ProposalListItemProps {
   account: string | null | undefined;
@@ -100,66 +91,4 @@ export function ProposalListItem({
       </div>
     </Card>
   );
-}
-
-interface BallotIconProps {
-  account: string | null | undefined;
-  proposalId: string;
-  isGSCProposal?: boolean;
-}
-function BallotIcon({
-  account,
-  proposalId,
-  isGSCProposal,
-}: BallotIconProps): ReactElement | null {
-  const { data: ballot } = useBallot(account, proposalId);
-  const { data: gscBallot } = useGSCBallot(account, proposalId);
-
-  if (!isGSCProposal && ballot === undefined) {
-    return null;
-  }
-  if (isGSCProposal && gscBallot === undefined) {
-    return null;
-  }
-
-  const [votingPowerBN, coreCastBallot] = ballot ?? [];
-  const [gscVotingPowerBN, gscCastBallot] = gscBallot ?? [];
-  let castBallot = coreCastBallot;
-  let votingPower = Number(formatEther(votingPowerBN || 0));
-  if (isGSCProposal) {
-    votingPower = gscVotingPowerBN?.toNumber() || 0;
-    castBallot = gscCastBallot;
-  }
-
-  if (votingPower && castBallot === Ballot.YES) {
-    return (
-      <div className="h-4 w-4 text-green-500">
-        <Tooltip content={t`Voted yes`}>
-          <ThumbUpIcon height="18" />
-        </Tooltip>
-      </div>
-    );
-  }
-
-  if (votingPower && castBallot === Ballot.NO) {
-    return (
-      <div className="h-4 w-4 text-red-500">
-        <Tooltip content={t`Voted no`}>
-          <ThumbDownIcon height="18" />
-        </Tooltip>
-      </div>
-    );
-  }
-
-  if (votingPower && castBallot === Ballot.MAYBE) {
-    return (
-      <div className="text-principalRoyalBlue h-4 w-4">
-        <Tooltip content={t`Voted abstain`}>
-          <XCircleIcon height="18" />
-        </Tooltip>
-      </div>
-    );
-  }
-
-  return null;
 }
