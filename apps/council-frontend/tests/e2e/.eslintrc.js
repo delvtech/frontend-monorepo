283ff4247118bb65069610d215b59b2115b9ffca
Duke <hnguyen1179@berkeley.edu>
2022-05-09 15:18:34 -0700

    Add rules to e2e eslint

--- apps/council-frontend/tests/e2e/.eslintrc.js
+++ apps/council-frontend/tests/e2e/.eslintrc.js
@@ -6,4 +6,9 @@ const synpressPath = path.join(
 
 module.exports = {
   extends: `${synpressPath}/.eslintrc.js`,
+  rules: {
+    "ui-testing/no-css-page-layout-selector": ["warn", "cypress"],
+    "testing-library/await-async-query": "off",
+    "testing-library/prefer-screen-queries": "off",
+  },
 };
