a16dddcee8d0a21ddccd08eae45df69c8a40ae27
Matthew Brown <mattfdbrown@gmail.com>
2022-05-10 10:25:14 -0500

    add task

--- apps/council-testnet/hardhat.config.ts
+++ apps/council-testnet/hardhat.config.ts
@@ -1,16 +1,16 @@
+/* eslint-disable no-console */
 // This adds ethers to the hre which has dev utilities for local testnet like 'getSigners()'
 import "@nomiclabs/hardhat-waffle";
 // Typechain support for hardhat
 import "@typechain/hardhat";
+import "dotenv/config";
 // Ethernal plugin - a blockchain / contract explorer for private testnets
 import "hardhat-ethernal";
 // This adds support for typescript paths mappings
 import "tsconfig-paths/register";
 
-import "dotenv/config";
-
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { getTokenList } from "@elementfi/elf-council-tokenlist";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers, providers } from "ethers";
 import fs from "fs";
 import {
@@ -20,9 +20,10 @@ import {
   types,
 } from "hardhat/config";
 
-import { createProposal } from "src/scripts/createProposal";
+import { createGoerliGscProposal } from "src/scripts/createGoerliGscProposal";
 import { createGoerliProposal } from "src/scripts/createGoerliProposal";
 import { createGscProposal } from "src/scripts/createGscProposal";
+import { createProposal } from "src/scripts/createProposal";
 
 const { PROPOSER_PRIVATE_KEY } = process.env;
 const LOCAL_RPC_HOST = "http://127.0.0.1:8545";
@@ -207,6 +208,48 @@ task("intervalMining", "Mine blocks on an interval")
     await localhostProvider.send("evm_setIntervalMining", [interval]);
   });
 
+task("createGoerliGscProposal", "Creates a new gsc proposal on goerli")
+  .addOptionalParam(
+    "ballot",
+    "How the proposer will vote, YES (0), NO (1), MAYBE (2)",
+    2,
+    types.int,
+  )
+  .addOptionalParam(
+    "expired",
+    "If the proposal should be immediately expired",
+    false,
+    types.boolean,
+  )
+  .setAction(async (taskArgs: { ballot: number; expired: boolean }) => {
+    const { ballot, expired } = taskArgs;
+    console.log("expired", expired);
+    console.log("ballot", ballot);
+
+    if (!PROPOSER_PRIVATE_KEY) {
+      console.log("ERROR: no private key provided for proposer");
+      return;
+    }
+
+    const goerliProvider = new providers.JsonRpcProvider(
+      ALCHEMY_GOERLI_RPC_HOST,
+    );
+
+    const owner = new ethers.Wallet(PROPOSER_PRIVATE_KEY, goerliProvider);
+
+    const ownerAddress = owner.address;
+    console.log("ownerAddress", ownerAddress);
+
+    await createGoerliGscProposal(
+      owner as unknown as SignerWithAddress,
+      goerliProvider,
+      {
+        ballot,
+        expired,
+      },
+    );
+  });
+
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   paths: {
@@ -252,6 +295,9 @@ const config: HardhatUserConfig = {
     },
     mainnet: {
       url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+    },
+    goerli: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_GOERLI_API_KEY}`,
     },
   },
 };
