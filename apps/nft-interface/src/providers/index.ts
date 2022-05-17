85ae624765273b685b72eacbfbe5515646efdb91
Danny Delott <dannydelott@gmail.com>
2022-05-16 17:12:57 -0700

    Update env for nft-interface

--- apps/nft-interface/src/providers/index.ts
+++ apps/nft-interface/src/providers/index.ts
@@ -9,11 +9,10 @@ export const ALCHEMY_GOERLI_KEY = process.env
 export const ALCHEMY_MAINNET_KEY = process.env
   .NEXT_PUBLIC_MAINNET_ALCHEMY_KEY as string;
 
-export const ALCHEMY_GOERLI_HTTP_URL = `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_GOERLI_KEY}`;
-export const ALCHEMY_MAINNET_HTTP_URL = `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_MAINNET_KEY}`;
-
-export const ALCHEMY_GOERLI_WSS_URL = `wss://eth-goerli.ws.alchemyapi.io/v2/${ALCHEMY_GOERLI_KEY}`;
-export const ALCHEMY_MAINNET_WSS_URL = `wss://eth-mainnet.ws.alchemyapi.io/v2/${ALCHEMY_GOERLI_KEY}`;
+export const ALCHEMY_GOERLI_HTTP_URL = process.env
+  .NEXT_PUBLIC_GOERLI_URI as string;
+export const ALCHEMY_MAINNET_HTTP_URL = process.env
+  .NEXT_PUBLIC_MAINNET_URI as string;
 
 export const NEXT_ENV = process.env.NODE_ENV as string;
 const LOCAL_RPC_HOST = "http://127.0.0.1:8545";
@@ -21,21 +20,21 @@ const LOCAL_RPC_HOST = "http://127.0.0.1:8545";
 export const getProvider = memoize(() => {
   const chainId = getTargetChain();
   if (chainId === ChainId.GOERLI) {
-    const alchemyWeb3GoerliWebSocketProvider = new providers.Web3Provider(
-      createAlchemyWeb3(ALCHEMY_GOERLI_WSS_URL)
+    const alchemyWeb3GoerliProvider = new providers.Web3Provider(
+      createAlchemyWeb3(ALCHEMY_GOERLI_HTTP_URL)
         .currentProvider as ExternalProvider,
       ChainId.GOERLI,
     );
-    return alchemyWeb3GoerliWebSocketProvider;
+    return alchemyWeb3GoerliProvider;
   }
 
   if (chainId === ChainId.MAINNET) {
-    const alchemyWeb3MainnetWebSocketProvider = new providers.Web3Provider(
-      createAlchemyWeb3(ALCHEMY_MAINNET_WSS_URL)
+    const alchemyWeb3MainnetProvider = new providers.Web3Provider(
+      createAlchemyWeb3(ALCHEMY_MAINNET_HTTP_URL)
         .currentProvider as ExternalProvider,
       ChainId.MAINNET,
     );
-    return alchemyWeb3MainnetWebSocketProvider;
+    return alchemyWeb3MainnetProvider;
   }
 
   console.warn("Chain id not recognized. Defaulting to local host provider.");
