a9a29453e85b86243910c0fdd44493bf3373602b
Danny Delott <dannydelott@gmail.com>
2022-05-19 16:33:01 -0700

    Fix nft-interface workspace imports

--- apps/nft-interface/next.config.js
+++ apps/nft-interface/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    externalDir: true,
+  },
   images: {
     domains: ["assets.example.com", "element-fi.mypinata.cloud", "ipfs.io"],
   },
