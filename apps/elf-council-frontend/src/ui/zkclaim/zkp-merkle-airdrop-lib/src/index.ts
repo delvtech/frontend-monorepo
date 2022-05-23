c1bc4a3ec48693c65d75bbe1d431c68d7788bd26
Duke <hnguyen1179@berkeley.edu>
2022-05-23 10:17:23 -0700

    Fix paths

--- apps/elf-council-frontend/src/ui/zkclaim/zkp-merkle-airdrop-lib/src/index.ts
+++ apps/elf-council-frontend/src/ui/zkclaim/zkp-merkle-airdrop-lib/src/index.ts
@@ -1,8 +1,9 @@
-export { MerkleTree, MerkleProof, TreeNode } from "./MerkleTree";
+export { MerkleTree, TreeNode } from "zkp-merkle-airdrop-lib/src/MerkleTree";
+export type { MerkleProof } from "zkp-merkle-airdrop-lib/src/MerkleTree";
 export {
   generateProofCallData,
   toHex,
   mimcSponge,
   pedersenHash,
   pedersenHashConcat,
-} from "./Library";
+} from "zkp-merkle-airdrop-lib/src/Library";
