e13a90ae78e3cf8bba99b9eec0152797581273d9
Matthew Brown <mattfdbrown@gmail.com>
2022-05-26 12:52:56 -0500

    copy sdk-v2 to sdk-council

---
+++ packages/sdk-council/README.md
@@ -0,0 +1,3 @@
+# Element SDK-V2
+
+A software development kit for Element Finance V2 contracts
---
+++ packages/sdk-council/hardhat.config.ts
@@ -0,0 +1,23 @@
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "tsconfig-paths/register";
+
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+dotenv.config();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  mocha: { timeout: 0 },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+  },
+};
+
+export default config;
