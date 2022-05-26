49382ff81b0552d27175626ac054dea47afbec9a
Cash DeLeon <cashd@users.noreply.github.com>
2022-05-26 17:22:27 -0500

    Refactor useVotingPowerByDelegates to use query filters for better args typing

--- apps/elf-council-frontend/src/ui/gsc/useVotingPowerByDelegates.ts
+++ apps/elf-council-frontend/src/ui/gsc/useVotingPowerByDelegates.ts
@@ -1,55 +1,69 @@
-import { BigNumber, Event } from "ethers";
-import { useSmartContractEvents } from "@elementfi/react-query-typechain/src/hooks/useSmartContractEvents/useSmartContractEvents";
+import { BigNumber } from "ethers";
 import { lockingVaultContract, vestingContract } from "src/elf/contracts";
-import { useMemo } from "react";
 import { getFromBlock } from "src/elf-council-addresses/getFromBlock";
+import { useQuery, UseQueryResult } from "react-query";
+
+const oneAddress =
+  "0x0000000000000000000000000000000000000000000000000000000000000001";
 
 export type VotePowerByDelegate = Record<string, BigNumber>;
 
-export function useVotingPowerByDelegates(): VotePowerByDelegate {
-  const { data: vestingVaultEvents = [] } = useSmartContractEvents(
-    vestingContract,
-    "VoteChange",
-    {
-      fromBlock: getFromBlock(),
-    },
-  );
-
-  const { data: lockingVaultEvents = [] } = useSmartContractEvents(
-    lockingVaultContract,
-    "VoteChange",
-    {
-      fromBlock: getFromBlock(),
-    },
-  );
-
-  const events = [...lockingVaultEvents, ...vestingVaultEvents];
-  // Using the length here is a better hook dependency
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  return useMemo(() => getVotePowerByDelegate(events), [events.length]);
-}
-
 /**
  * Returns a record of all delegates and their respective voting power
- * @returns {VotePowerByDelegate} a mapping between delegate address -> voting power
+ * @returns {Promise<VotePowerByDelegate>} a mapping between delegate address -> voting power
  */
-function getVotePowerByDelegate(events: Event[]): VotePowerByDelegate {
-  const votePowerByDelegates: VotePowerByDelegate = {};
+async function getVotingPowerByDelegates(): Promise<VotePowerByDelegate> {
+  // Filters for all vote change events
+  const lockingFilter = lockingVaultContract.filters.VoteChange(
+    null,
+    null,
+    null,
+  );
+  const vestingFilter = vestingContract.filters.VoteChange(null, null, null);
 
-  events.forEach((event) => {
-    const [, delegate, amount]: [string, string, BigNumber] = event.args as [
-      string,
-      string,
-      BigNumber,
-    ];
+  // Query for events
+  const lockingEvents = await lockingVaultContract.queryFilter(
+    lockingFilter,
+    getFromBlock(),
+  );
 
-    if (delegate in votePowerByDelegates) {
-      votePowerByDelegates[delegate] =
-        votePowerByDelegates[delegate].add(amount);
-    } else {
-      votePowerByDelegates[delegate] = amount;
+  const vestingEvents = await vestingContract.queryFilter(
+    vestingFilter,
+    getFromBlock(),
+  );
+
+  const allEvents = lockingEvents.concat(vestingEvents);
+  const sortedEvents = allEvents.sort(
+    (eventA, eventB) => eventA.blockNumber - eventB.blockNumber,
+  );
+
+  const votePowerByDelegate: VotePowerByDelegate = {};
+
+  // Compute total voting power state of voting vaults
+  sortedEvents.forEach((event) => {
+    if (event.args) {
+      const from = event.args.from;
+      const to = event.args.to;
+      const amount = event.args.amount;
+
+      // Disregard events to and from one address. This is a reserved address for certain vesting vault events.
+      if (to === oneAddress || from === oneAddress) {
+        return;
+      }
+
+      if (to in votePowerByDelegate) {
+        votePowerByDelegate[to] = votePowerByDelegate[to].add(amount);
+      } else {
+        votePowerByDelegate[to] = amount;
+      }
     }
   });
 
-  return votePowerByDelegates;
+  return votePowerByDelegate;
+}
+
+export function useVotingPowerByDelegates(): UseQueryResult<VotePowerByDelegate> {
+  return useQuery("voting-power-delegates", async () => {
+    return await getVotingPowerByDelegates();
+  });
 }
