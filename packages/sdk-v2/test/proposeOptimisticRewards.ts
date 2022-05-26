2c9c33b4ff1081b674ab340d57d14de3115dfc1e
Matthew Brown <mattfdbrown@gmail.com>
2022-05-26 12:52:14 -0500

    move merkle to base

--- packages/sdk-v2/test/proposeOptimisticRewards.ts
+++ packages/sdk-v2/test/proposeOptimisticRewards.ts
@@ -10,7 +10,7 @@ import MerkleTree from "merkletreejs";
 
 import { proposeOptimisticRewards } from "src/proposeOptimisticRewards";
 
-import { Account, getMerkleTree } from "./merkle";
+import { Account, getMerkleTree } from "@elementfi/base/merkle/merkle";
 
 import { MockERC20, MockERC20__factory } from "@elementfi/council-typechain";
 
