25266184d114606736424d659cba31103f0ee2a7
Cash Baller <cashd@users.noreply.github.com>
2022-05-26 17:58:26 -0500

    GSC UI - Fix rising delegates and flashing kick button on init (#162)
    * fix gscmember row from flashing kick state
    
    Ideally we should want access to loading state from voting power vault hooks
    
    * Refactor useVotingPowerByDelegates to use query filters for better args typing
    
    * move top 20 candidates logic from helper function to hook directly
    
    * Implement refactored hooks into overview page and added spinner for rising loading state

--- apps/elf-council-frontend/src/ui/gsc/useCandidates.tsx
+++ apps/elf-council-frontend/src/ui/gsc/useCandidates.tsx
@@ -6,6 +6,8 @@ import {
   useVotingPowerByDelegates,
   VotePowerByDelegate,
 } from "src/ui/gsc/useVotingPowerByDelegates";
+import { useGSCVotePowerThreshold } from "./useGSCVotePowerThreshold";
+import { BigNumber } from "ethers";
 
 /**
  * Returns a list of candidates, sorted by highest to lowest voting power
@@ -14,11 +16,15 @@ import {
 export function useGSCCandidates(): Delegate[] {
   const { data: members = [] } = useGSCMembers();
   const gscMemberAddresses = members.map(({ address }) => address);
+  const { data: threshold } = useGSCVotePowerThreshold();
 
-  const votePowerByDelegates = useVotingPowerByDelegates();
+  const { data: votePowerByDelegates = {} } = useVotingPowerByDelegates();
 
   return useMemo(
-    () => sortVotingPower(votePowerByDelegates, gscMemberAddresses),
+    () =>
+      threshold
+        ? sortVotingPower(votePowerByDelegates, gscMemberAddresses, threshold)
+        : [],
     // Using the length here is a better hook dependency
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [gscMemberAddresses.length, votePowerByDelegates],
@@ -32,16 +38,22 @@ export function useGSCCandidates(): Delegate[] {
 function sortVotingPower(
   votePowerByDelegates: VotePowerByDelegate,
   gscMembers: Array<string>,
+  threshold: BigNumber,
 ) {
   const knownDelegates = delegates;
 
   return Object.entries(votePowerByDelegates)
     .sort((a, b) => Number(formatEther(b[1])) - Number(formatEther(a[1])))
-    .filter(([address]) => !gscMembers.includes(address))
+    .filter(([address]) => {
+      const votingPower = votePowerByDelegates[address];
+      return !gscMembers.includes(address) && votingPower.lt(threshold);
+    })
     .map(([address]) => {
       const knownDelegate = knownDelegates.find(
         (delegate) => address === delegate.address,
       );
+
       return knownDelegate ?? { address };
-    });
+    })
+    .slice(0, 20);
 }
