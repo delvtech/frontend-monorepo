470b0f960bc1488dccf313979410ebe19d67b684
Matthew Brown <mattfdbrown@gmail.com>
2022-06-08 09:17:11 -0500

    remove merkle info for voting

--- apps/council-frontend/src/ui/voting/useVote.ts
+++ apps/council-frontend/src/ui/voting/useVote.ts
@@ -1,5 +1,6 @@
 import { useCallback } from "react";
 
+import { CoreVoting } from "@elementfi/council-typechain";
 import {
   useSmartContractTransaction,
   UseSmartContractTransactionOptions,
@@ -10,12 +11,10 @@ import { ethers, Signer } from "ethers";
 import { addressesJson } from "src/elf-council-addresses";
 import { coreVotingContract } from "src/elf/contracts";
 import { MerkleProof } from "src/elf/merkle/MerkleProof";
-import { MerkleRewardType, useMerkleInfo } from "src/elf/merkle/useMerkleInfo";
 import { useLatestBlockNumber } from "src/ui/ethereum/useLatestBlockNumber";
 import { Ballot } from "src/ui/voting/Ballot";
 import { useLockingVaultVotingPower } from "src/ui/voting/useLockingVaultVotingPower";
 import { useVestingVaultVotingPower } from "src/ui/voting/useVestingVaultVotingPower";
-import { CoreVoting } from "@elementfi/council-typechain";
 
 const { lockingVault: lockingVaultAddress, vestingVault: vestingVaultAddress } =
   addressesJson.addresses;
@@ -32,7 +31,6 @@ export function useVote(
   isError: boolean;
 } {
   const { data: latestBlockNumber } = useLatestBlockNumber();
-  const { data: merkleInfo } = useMerkleInfo(account, MerkleRewardType.RETRO);
 
   const {
     mutate: vote,
@@ -55,7 +53,7 @@ export function useVote(
     (proposalId: string, ballot: Ballot) => {
       const blockNumber = proposalCreatedAtBlockNumber || latestBlockNumber;
 
-      if (!blockNumber || !merkleInfo || !account) {
+      if (!blockNumber || !account) {
         return;
       }
 
@@ -83,7 +81,6 @@ export function useVote(
       proposalCreatedAtBlockNumber,
       latestBlockNumber,
       lockingVaultVotingPower,
-      merkleInfo,
       vestingVaultVotingPower,
       vote,
     ],
