c666f14a42da0ec611aeddeeebf514489b4603b8
Duke Nguyen <hnguyen1179@berkeley.edu>
2022-06-10 15:16:08 -0700

    Add eslint-config package (#185)
    * Eslint initial config
    
    * Add eslint and eslint plugins
    
    * Add eslint and eslintrc
    
    * Run yarn
    
    * Touch up rule exceptions
    
    * Configure eslintrc for base package
    
    * Add precommit
    
    * Test
    
    * Undo test changes

--- packages/base/debug/useConsole.ts
+++ packages/base/debug/useConsole.ts
@@ -3,16 +3,17 @@ import { usePrevious } from "react-use";
 
 import isEqual from "lodash.isequal";
 
-/* eslint-disable */
 /**
  * Hook to only console.log when the inputs to console.log change.
  * @param consoleArgs arguments to console.log
  */
-export function useConsole(...consoleArgs: any) {
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function useConsole(...consoleArgs: any): void {
   const previousArgs = usePrevious(consoleArgs);
   const changed = isEqual(previousArgs, consoleArgs);
   useEffect(() => {
+    // eslint-disable-next-line no-console
     console.log(...consoleArgs);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [changed]);
 }
-/* eslint-enable */
