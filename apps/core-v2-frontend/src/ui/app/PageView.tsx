a79aaa792671cc3a7e6af683573ebeb3156e1f55
Danny Delott <dannydelott@gmail.com>
2022-06-14 15:19:58 -0700

    Basic hero w/ cta for overview page

--- apps/core-v2-frontend/src/ui/app/PageView.tsx
+++ apps/core-v2-frontend/src/ui/app/PageView.tsx
@@ -10,7 +10,7 @@ export default function PageView(props: PageViewProps): ReactElement {
   const { children, childrenContainerClassName } = props;
   return (
     <Fragment>
-      <div className="bg-appBackgroundLight min-h-screen">
+      <div className="min-h-screen bg-base-200">
         <div className="w-full p-6">
           <div className={classNames("mt-6", childrenContainerClassName)}>
             {children}
--- apps/core-v2-frontend/src/ui/overview/OverviewPage.tsx
+++ apps/core-v2-frontend/src/ui/overview/OverviewPage.tsx
@@ -1,7 +1,21 @@
 import React, { ReactElement } from "react";
 
 export function OverviewPage(): ReactElement {
-  return <button className="daisy-btn">Connect wallet</button>;
+  return (
+    <div className="daisy-hero min-h-screen bg-base-200">
+      <div className="daisy-hero-content text-center">
+        <div>
+          <h1 className="text-5xl font-bold">
+            Connect your wallet to continue
+          </h1>
+          <p className="py-6"></p>
+          <button className="daisy-btn daisy-btn-primary">
+            Connect wallet
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default OverviewPage;
