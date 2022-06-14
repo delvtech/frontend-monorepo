6aa3979ffa0ff9f7a13319b1910d953ce51e1742
Danny Delott <dannydelott@gmail.com>
2022-06-14 14:40:29 -0700

    add comments to daisy config

--- apps/core-v2-frontend/tailwind.config.js
+++ apps/core-v2-frontend/tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   plugins: [require("@tailwindcss/forms"), require("daisyui")],
+  // Use daisyui for now so we can prototype using predefined tailwind classes, eg: .daisy-btn
   daisyui: {
+    // brutalist wireframing theme while we prototype
     themes: ["wireframe"],
+
+    // Prefix all daisy- classes so they are easy to search/find globally
     prefix: "daisy-",
   },
+
   // allows tailwind to tree shake all css not included in the files found in this array.
   content: [
     // .ts allow for long tailwind classes to be extract to their own files.
