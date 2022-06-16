6a733919e35b2a5707d6c5e6378f982647ca50bc
Danny Delott <dannydelott@gmail.com>
2022-06-16 11:44:43 -0700

    refactor to delegates package

--- apps/council-frontend/src/elf-council-delegates/delegates.ts
+++ apps/council-frontend/src/elf-council-delegates/delegates.ts
@@ -1,50 +1,28 @@
 import { addressesJson } from "src/addresses";
 import { ChainId } from "@elementfi/base/ethereum/ethereum";
 
-import testnetDelegatesJson from "src/elf-council-delegates/testnet.delegates.json";
-import mainnetDelegatesJson from "src/elf-council-delegates/mainnet.delegates.json";
-// extra delegates that weren't scrape-able by our current script, these had to be hardcoded :(
-import mainnetExtrasDelegatesJson from "src/elf-council-delegates/mainnet-extras.delegates.json";
-import goerliDelegatesJson from "src/elf-council-delegates/goerli.delegates.json";
-
-export interface DelegatesJson {
-  chainId: number;
-  version: string;
-  delegates: Delegate[];
-}
-
-export interface Delegate {
-  address: string;
-  commonwealthPostedFromAddress?: string;
-  commonwealthCommentId?: number;
-  commonwealthName?: string | null;
-  name?: string;
-  description?: string;
-
-  /**
-   * Twitter handle w/out the @ symbol, eg: "CharlieStLouis" (not "@CharlieStLouis")
-   */
-  twitterHandle?: string;
-}
+import {
+  mainnetDelegates,
+  goerliDelegates,
+  testnetDelegates,
+  Delegate,
+} from "@elementfi/council-delegates";
 
 export const delegates = getDelegates();
 
 function getDelegates(): Delegate[] {
   if (addressesJson.chainId === ChainId.GOERLI) {
-    return goerliDelegatesJson.delegates;
+    return goerliDelegates;
   }
 
   if (addressesJson.chainId === ChainId.MAINNET) {
-    return [
-      ...mainnetDelegatesJson.delegates,
-      ...mainnetExtrasDelegatesJson.delegates,
-    ];
+    return mainnetDelegates;
   }
 
   if (addressesJson.chainId === ChainId.LOCAL) {
-    return testnetDelegatesJson.delegates;
+    return testnetDelegates;
   }
 
   // Should never happen, but for type safety we default to hardhat testnet delegates
-  return testnetDelegatesJson.delegates;
+  return testnetDelegates;
 }
