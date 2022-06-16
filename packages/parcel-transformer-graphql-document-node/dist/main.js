1453eea8bcbcbe95d0f1bf19b2bbb7cab1254756
Ryan Goree <goree.ryan@gmail.com>
2022-06-16 11:12:01 -0500

    Add comment

--- packages/parcel-transformer-graphql-document-node/dist/main.js
+++ packages/parcel-transformer-graphql-document-node/dist/main.js
@@ -21,16 +21,26 @@ $parcel$export(
   () => $ccf926d2e52e99d1$export$2e2bcd8739ae039,
 );
 
-var $ccf926d2e52e99d1$export$2e2bcd8739ae039 = new (0,
-$fo5Ih$parcelplugin.Transformer)({
-  async transform({ asset: asset }) {
-    const code = await asset.getCode();
-    const source = new (0, $fo5Ih$graphql.Source)(code, asset.filePath);
-    const document = (0, $fo5Ih$graphql.parse)(source);
-    asset.type = "js";
-    asset.setCode(`module.exports=${JSON.stringify(document)};`);
-    return [asset];
-  },
-});
+var /**
+   * A custom Parcel transformer that converts GraphQL files into `DocumentNode`s
+   *
+   * This is needed because the plugin that is automatically installed by Parcel
+   * for GraphQL files will cause the build to fail if the GraphQL doesn't
+   * include any operations (e.g., queries, mutations).
+   *
+   * An important thing to note is that this transformer does not load other
+   * GraphQL fragments imported with the `# import` syntax, while the default
+   * GraphQL transformer does.
+   */ $ccf926d2e52e99d1$export$2e2bcd8739ae039 = new (0,
+  $fo5Ih$parcelplugin.Transformer)({
+    async transform({ asset: asset }) {
+      const code = await asset.getCode();
+      const source = new (0, $fo5Ih$graphql.Source)(code, asset.filePath);
+      const document = (0, $fo5Ih$graphql.parse)(source);
+      asset.type = "js";
+      asset.setCode(`module.exports=${JSON.stringify(document)};`);
+      return [asset];
+    },
+  });
 
 //# sourceMappingURL=main.js.map
--- packages/parcel-transformer-graphql-document-node/index.ts
+++ packages/parcel-transformer-graphql-document-node/index.ts
@@ -1,6 +1,17 @@
 import { Transformer } from "@parcel/plugin";
 import { parse, Source } from "graphql";
 
+/**
+ * A custom Parcel transformer that converts GraphQL files into `DocumentNode`s
+ *
+ * This is needed because the plugin that is automatically installed by Parcel
+ * for GraphQL files will cause the build to fail if the GraphQL doesn't
+ * include any operations (e.g., queries, mutations).
+ *
+ * An important thing to note is that this transformer does not load other
+ * GraphQL fragments imported with the `# import` syntax, while the default
+ * GraphQL transformer does.
+ */
 export default new Transformer({
   async transform({ asset }) {
     const code = await asset.getCode();
