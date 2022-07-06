4b487773fe0ec5695e747f0bf51b6c23397589a8
Cash DeLeon <cashd@users.noreply.github.com>
2022-07-06 16:35:09 -0500

    added  eslint and prettier configs from packages

--- apps/core-v2-testnet/.eslintrc.js
+++ apps/core-v2-testnet/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     "standard",
     "plugin:prettier/recommended",
     "plugin:node/recommended",
+    "@elementfi/eslint-config",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
