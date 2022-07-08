fd14c09d29f8f4674b3aace97be0c10312cbd15a
Ryan Goree <goree.ryan@gmail.com>
2022-07-08 17:38:14 -0500

    Turn council-graphql server off in prod

--- apps/council-frontend/pages/api/graphql.ts
+++ apps/council-frontend/pages/api/graphql.ts
@@ -2,9 +2,11 @@ import { createServer } from "@elementfi/graphql";
 import { councilGraph } from "@elementfi/council-graphql";
 import { defaultProvider } from "src/providers/providers";
 
-const server = createServer({
-  graphs: [councilGraph],
-  provider: defaultProvider,
-});
+const server =
+  process.env.NODE_ENV === "development" &&
+  createServer({
+    graphs: [councilGraph],
+    provider: defaultProvider,
+  });
 
 export default server;
