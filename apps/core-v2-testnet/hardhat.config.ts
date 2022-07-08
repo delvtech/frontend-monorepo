3066219a2c673d09a93520aff47cc7e15f8c67bf
Cash Baller <cashd@users.noreply.github.com>
2022-07-08 15:45:12 -0500

    Upgrade Hardhat to 2.10, reset project to use new boilerplate (#241)
    * Upgrade Hardhat to 2.10, reset project to use new boilerplate
    
    * Update package.json

--- apps/core-v2-testnet/hardhat.config.ts
+++ apps/core-v2-testnet/hardhat.config.ts
@@ -1,13 +1,5 @@
-import * as dotenv from "dotenv";
-
 import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-
-dotenv.config();
+import "@nomicfoundation/hardhat-toolbox";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,11 +10,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
--- apps/core-v2-testnet/scripts/deploy.ts
+++ apps/core-v2-testnet/scripts/deploy.ts
@@ -1,25 +1,18 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const lockedAmount = ethers.utils.parseEther("1");
 
-  await greeter.deployed();
+  const Lock = await ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  console.log("Greeter deployed to:", greeter.address);
+  await lock.deployed();
+
+  console.log("Lock with 1 ETH deployed to:", lock.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
