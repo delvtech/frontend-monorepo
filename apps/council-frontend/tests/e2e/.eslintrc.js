974e1e99d8466c82286bb4cb80b2c5ba77f09a5b
Ryan Goree <goree.ryan@gmail.com>
2022-07-29 15:11:41 -0500

    Bump eslint in council-frontend and modify e2e testing eslint config to ignore root config

--- apps/council-frontend/tests/e2e/.eslintrc.js
+++ apps/council-frontend/tests/e2e/.eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
+  ignorePatterns: ["*.json", "*.md"],
   extends: require.resolve("@synthetixio/synpress/.eslintrc.js"),
+  root: true,
   rules: {
     "ui-testing/no-css-page-layout-selector": ["warn", "cypress"],
     "testing-library/await-async-query": "off",
