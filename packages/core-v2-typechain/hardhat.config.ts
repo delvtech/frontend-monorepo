bfcbd86f0880650f45469928293c75aff97517fc
Ryan Goree <goree.ryan@gmail.com>
2022-07-29 11:24:08 -0500

    Cleanup configs

--- packages/core-v2-typechain/hardhat.config.ts
+++ packages/core-v2-typechain/hardhat.config.ts
@@ -21,7 +21,7 @@ const config: HardhatUserConfig = {
     ],
   },
   typechain: {
-    outDir: "types",
+    outDir: "compiled",
     target: "ethers-v5",
     alwaysGenerateOverloads: true,
     externalArtifacts: ["externalArtifacts/*.json"],
