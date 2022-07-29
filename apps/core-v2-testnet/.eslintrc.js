9491a964b54c76398f31e0de6894234f9b808b6a
Cash DeLeon <cashd@users.noreply.github.com>
2022-07-29 15:49:01 -0500

    fixed ts path and eslint issue
    turned off node rule in favor of using eslint import plugin for coverage

--- apps/core-v2-testnet/.eslintrc.js
+++ apps/core-v2-testnet/.eslintrc.js
@@ -6,7 +6,11 @@ module.exports = {
     node: true,
   },
   // extends from monorepo eslints package
-  extends: ["@elementfi/eslint-config", "plugin:node/recommended"],
+  extends: [
+    "plugin:node/recommended",
+    "plugin:import/recommended",
+    "@elementfi/eslint-config",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
@@ -17,7 +21,7 @@ module.exports = {
       { ignores: ["modules"] },
     ],
     // TODO @cashd: Changed rule to warning until I can figure out resolving ts paths
-    "node/no-missing-import": ["warn"],
+    "node/no-missing-import": "off",
     // disabling this rule because the testnet will need to output to log frequently
     "no-console": "off",
     "no-process-exit": "off",
