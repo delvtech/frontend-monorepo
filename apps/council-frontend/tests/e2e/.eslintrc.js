2555960dc019bd4fb133b5ec4a46ca58e39cff9a
Ryan Goree <goree.ryan@gmail.com>
2022-07-29 14:12:38 -0500

    Simplify synpress eslintrc path resolution in council-frontend

--- apps/council-frontend/tests/e2e/.eslintrc.js
+++ apps/council-frontend/tests/e2e/.eslintrc.js
@@ -1,25 +1,5 @@
-const path = require("path");
-
-/**
- * Hacky way to get the absolute project root path:
- *
- * process.cwd() (Current Working Directory)
- * Returns a different path depending on where the script that calls it is ran from
- *
- * This is a solution for when the script is in the council-frontend or root dir
- */
-let ROOT_PATH = process.cwd().slice(0, process.cwd().indexOf("/apps"));
-if (ROOT_PATH.charAt(ROOT_PATH.length - 1) !== "o") {
-  ROOT_PATH += "o";
-}
-
-const synpressPath = path.join(
-  ROOT_PATH,
-  "/node_modules/@synthetixio/synpress",
-);
-
 module.exports = {
-  extends: `${synpressPath}/.eslintrc.js`,
+  extends: require.resolve("@synthetixio/synpress/.eslintrc.js"),
   rules: {
     "ui-testing/no-css-page-layout-selector": ["warn", "cypress"],
     "testing-library/await-async-query": "off",
