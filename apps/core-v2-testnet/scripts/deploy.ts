129f5f3cdfc750e94ad7dfa350ac401060081ea4
Cash DeLeon <cashd@users.noreply.github.com>
2022-08-01 17:29:40 -0500

    term deploy script init

--- apps/core-v2-testnet/scripts/deploy.ts
+++ apps/core-v2-testnet/scripts/deploy.ts
@@ -1,21 +1,43 @@
 import { ethers } from "hardhat";
 import Logger from "utils/logger";
 
+import {
+  ForwarderFactory__factory,
+  MockERC20__factory,
+  ERC4626Term__factory,
+  MockERC4626__factory,
+} from "@elementfi/core-v2-typechain";
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const signers = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const [signer] = signers;
 
-  const Lock = await ethers.getContractFactory("Lock");
+  const forwarderFactoryFactory = new ForwarderFactory__factory(signer);
+  const forwarderFactory = await forwarderFactoryFactory.deploy();
+  await forwarderFactory.deployed();
 
-  Logger.deployContract("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const erc20Factory = new MockERC20__factory(signer);
+  const mockERC20 = await erc20Factory.deploy("CASH", "CASH", 18);
+  await mockERC20.deployed();
 
-  await lock.deployed();
+  const vaultFactory = new MockERC4626__factory(signer);
+  const yearnVault = await vaultFactory.deploy(mockERC20.address);
+  await yearnVault.deployed();
 
-  Logger.successfulDeploy("Lock", lock);
+  const termFactory = new ERC4626Term__factory(signer);
+  // ERC20Forwarder contrat bytecode hash
+  const linkHash = await forwarderFactory.ERC20LINK_HASH();
+  const maxReserve = ethers.utils.parseEther("50000");
+  const owner = signer.address;
+  const term = await termFactory.deploy(
+    yearnVault.address,
+    linkHash,
+    forwarderFactory.address,
+    maxReserve,
+    owner,
+  );
+  await term.deployed();
 }
 
 main().catch((error) => {
