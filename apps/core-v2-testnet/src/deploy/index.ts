5f95553e67471f5e7761f5a0d82912e34f650470
Cash DeLeon <cashd@users.noreply.github.com>
2022-08-03 14:51:51 -0500

    fix lint

--- apps/core-v2-testnet/src/deploy/index.ts
+++ apps/core-v2-testnet/src/deploy/index.ts
@@ -2,5 +2,5 @@ export * from "./deployForwarderFactory";
 export * from "./deployMockToken";
 export * from "./deployMockYearnVault";
 export * from "./deployMockYieldAdapter";
-export * from "./deployTermVault";
+export * from "./deploy4626Term";
 export * from "./deployVault";
