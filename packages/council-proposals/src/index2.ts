c1d35153af4d4aec2be1a0eb0ae5866098fac1e2
Danny Delott <dannydelott@gmail.com>
2022-08-04 14:46:22 -0700

    Fix imports

--- packages/council-proposals/src/index2.ts
+++ packages/council-proposals/src/index2.ts
@@ -1,4 +1,17 @@
-import testnetProposals from "./proposals/testnet.proposals.json";
-export { testnetProposals };
+import testnetProposals from "./proposals/json/testnet.proposals.json";
+import testnetGscProposals from "./proposals/json/testnet-gsc.proposals.json";
+import goerliProposals from "./proposals/json/goerli.proposals.json";
+import goerliGscProposals from "./proposals/json/goerli-gsc.proposals.json";
+import mainnetProposals from "./proposals/json/mainnet.proposals.json";
+import mainnetGscProposals from "./proposals/json/mainnet-gsc.proposals.json";
+
+export {
+  testnetProposals,
+  testnetGscProposals,
+  goerliProposals,
+  goerliGscProposals,
+  mainnetProposals,
+  mainnetGscProposals,
+};
 
 export * from "./proposals/types";
