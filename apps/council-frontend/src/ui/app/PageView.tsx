fcfda6b11ccba9bbd8548ccc76bc2dd19c447726
Duke <hnguyen1179@berkeley.edu>
2022-08-10 17:24:29 -0700

    Add network switching button to wrong network modal

--- apps/council-frontend/src/ui/app/PageView.tsx
+++ apps/council-frontend/src/ui/app/PageView.tsx
@@ -2,13 +2,14 @@ import React, { Fragment, ReactElement, ReactNode } from "react";
 import { ChainId, ChainNames } from "@elementfi/base";
 import classNames from "classnames";
 import { t } from "ttag";
-import { useAccount, useNetwork } from "wagmi";
+import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
 import { addressesJson } from "src/addresses";
 import Footer from "src/ui/app/Footer";
 import Header from "src/ui/app/Header";
 import Sidebar from "src/ui/app/Sidebar";
 import SimpleDialog from "src/ui/base/Dialog/Dialog";
 import H3 from "src/ui/base/H3/H3";
+import Button from "src/ui/base/Button/Button";
 
 interface PageViewProps {
   children?: ReactNode;
@@ -31,6 +32,7 @@ export default function PageView(props: PageViewProps): ReactElement {
   } = props;
   const { address } = useAccount();
   const { chain } = useNetwork();
+  const { switchNetwork, isLoading } = useSwitchNetwork();
 
   const isWrongChain = !!chain?.id && chain.id !== addressesJson.chainId;
 
@@ -59,12 +61,20 @@ export default function PageView(props: PageViewProps): ReactElement {
           {showFooter ? <Footer /> : null}
         </div>
       </div>
+
       <SimpleDialog isOpen={isWrongChain}>
-        <div className="text-center">
-          <H3>{t`Please connect to ${
-            ChainNames[addressesJson.chainId as ChainId]
-          }`}</H3>
-          <span>{t`Chain ID: ${addressesJson.chainId}`}</span>
+        <div className="flex flex-col text-center">
+          <H3>{t`Wrong network detected`}</H3>
+
+          <Button
+            onClick={() => switchNetwork?.(addressesJson.chainId)}
+            loading={isLoading}
+            className="mt-3 flex justify-center"
+          >
+            <span>
+              {`Switch to ${ChainNames[addressesJson.chainId as ChainId]}`}
+            </span>
+          </Button>
         </div>
       </SimpleDialog>
     </Fragment>
