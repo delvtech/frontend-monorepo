6fd70e15b7ca677f514f4aef8abfc36803f2f05b
Duke <hnguyen1179@berkeley.edu>
2022-08-11 17:05:06 -0700

    Remove deprecated code & rename connectors2.ts

--- apps/council-frontend/src/wallets/connectors2.ts
+++
@@ -1,7 +0,0 @@
-import { getDefaultWallets } from "@rainbow-me/rainbowkit";
-import { chains } from "src/provider";
-
-export const { connectors } = getDefaultWallets({
-  appName: "Element Finance Council Frontend",
-  chains,
-});
