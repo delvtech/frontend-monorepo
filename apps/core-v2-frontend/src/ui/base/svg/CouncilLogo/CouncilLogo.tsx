7d4683b65c7c3677c35e774ee19ed9592b52d961
Duke <hnguyen1179@berkeley.edu>
2022-08-12 14:57:28 -0700

    Remove unnecessary svg

--- apps/core-v2-frontend/src/ui/base/svg/CouncilLogo/CouncilLogo.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from "react";
-import { SVGComponentProps } from "src/ui/base/svg/types";
-import SVG from "./councilLogo.svg";
-
-export default function CouncilLogo(props: SVGComponentProps): ReactElement {
-  return <SVG {...props} />;
-}
