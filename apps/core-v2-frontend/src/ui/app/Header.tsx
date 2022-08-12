c42f88ab167b4a888fdc42d74a80e43ea85563e4
Duke <hnguyen1179@berkeley.edu>
2022-08-12 14:53:28 -0700

    Add element icon svg to header

--- apps/core-v2-frontend/src/ui/app/Header.tsx
+++ apps/core-v2-frontend/src/ui/app/Header.tsx
@@ -1,13 +1,17 @@
+import { ReactElement } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Navbar } from "@elementfi/component-library";
 
-import { ReactElement } from "react";
+import ElementIcon, {
+  IconSize,
+  IconVariant,
+} from "src/ui/base/svg/ElementIcon/ElementIcon";
 
 export default function Header(): ReactElement {
   return (
     <Navbar>
       <div className="flex-1">
-        <span className="text-xl font-bold normal-case">Element</span>
+        <ElementIcon variant={IconVariant.LIGHT} size={IconSize.LARGE} />
       </div>
       <div className="flex-none">{<ConnectButton />}</div>
     </Navbar>
