4f64da136bc7900f1dae1b702279b0f6943d2bc9
Duke <hnguyen1179@berkeley.edu>
2022-08-12 14:39:11 -0700

    Remove unused SVGs

--- apps/council-frontend/src/ui/base/svg/CoinbaseIcon/CoinbaseIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from "react";
-import { SVGComponentProps } from "src/ui/base/svg/types";
-import Svg from "./coinbasewallet.svg";
-
-export default function coinbaseIcon(props: SVGComponentProps): ReactElement {
-  return <Svg {...props} />;
-}
