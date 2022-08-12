2bdba7aa388637688e2a62a20aaf5a6b3c353de0
Danny Delott <dannydelott@gmail.com>
2022-08-12 11:41:07 -0700

    Add scripts for pushing to s3 and copying static files, remove old bash script, add new packages

---
+++ packages/council-delegates/scripts/push-to-s3.sh
@@ -0,0 +1,5 @@
+# mainnet
+aws s3 cp dist/json/mainnet.delegates.json s3://elementfi/mainnet.delegates.json
+
+# goerli 
+aws s3 cp dist/json/goerli.delegates.json s3://elementfi/goerli.delegates.json
