328dcd671f550f578c88e91e0258fe6988b3150f
Duke Nguyen <hnguyen1179@berkeley.edu>
2022-08-12 15:14:26 -0700

    Add svgs and type (#321)
    * Remove unused SVGs
    
    * Add base/svg folder + svgs + types
    
    * Add element icon svg to header
    
    * Remove unnecessary svg
    
    * Remove unnecessary file

--- apps/council-frontend/src/ui/base/svg/MetaMaskIcon/MetaMaskIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from "react";
-import { SVGComponentProps } from "src/ui/base/svg/types";
-import Svg from "./metaMaskIcon.svg";
-
-export default function MetaMaskIcon(props: SVGComponentProps): ReactElement {
-  return <Svg {...props} />;
-}
