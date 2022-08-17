e9595a09cacb5263555e60ea34c20dce3c26c6c4
Cash DeLeon <cashd@users.noreply.github.com>
2022-08-16 22:25:59 -0500

    create deployMockPool

--- apps/core-v2-testnet/src/deploy/index.ts
+++ apps/core-v2-testnet/src/deploy/index.ts
@@ -2,6 +2,7 @@ export * from "./deploy4626Term";
 export * from "./deployForwarderFactory";
 export * from "./deployVault";
 export * from "./mocks/deployMockPermitToken";
+export * from "./mocks/deployMockPool";
 export * from "./mocks/deployMockToken";
 export * from "./mocks/deployMockYearnVault";
 export * from "./mocks/deployMockYieldAdapter";
