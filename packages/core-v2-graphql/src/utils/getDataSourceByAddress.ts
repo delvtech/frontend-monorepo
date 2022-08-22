fe5ae4906bc100bccc5136e7aaf39607fe4eff97
Cash DeLeon <cashd@users.noreply.github.com>
2022-08-22 18:26:41 -0500

    better getDatasourcebyAddress function
    now infers key and gives proper return type

--- packages/core-v2-graphql/src/utils/getDataSourceByAddress.ts
+++ packages/core-v2-graphql/src/utils/getDataSourceByAddress.ts
@@ -1,11 +1,16 @@
 import { CoreV2Context } from "src/context";
 
+type CoreV2DataSources = CoreV2Context["elementDataSources"];
+
+type ArrElement<ArrType> = ArrType extends readonly (infer ElementType)[]
+  ? ElementType
+  : never;
+
 export function getDataSourceByAddress(
   address: string,
-  dataSources: CoreV2Context["elementDataSources"],
-  key: keyof CoreV2Context["elementDataSources"],
-): any | undefined {
-  return dataSources[key].find(
+  dataSources: CoreV2DataSources[keyof CoreV2DataSources],
+): ArrElement<CoreV2DataSources[keyof CoreV2DataSources]> | undefined {
+  return dataSources.find(
     (dataSource: { address: string }) => dataSource.address === address,
   );
 }
