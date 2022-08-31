51fcdc92138eb82f4ce2eb2c02b74b87b47f613a
Danny Delott <dannydelott@gmail.com>
2022-08-31 16:36:42 -0700

    Build component-library

--- packages/component-library/dist/js/index.esm.js
+++ packages/component-library/dist/js/index.esm.js
@@ -1,4 +1,4 @@
-import {jsxDEV as $aCh37$jsxDEV} from "react/jsx-dev-runtime";
+import {jsx as $aCh37$jsx, jsxs as $aCh37$jsxs} from "react/jsx-runtime";
 import $aCh37$classnames from "classnames";
 
 function $parcel$exportWildcard(dest, source) {
@@ -30,7 +30,7 @@ $parcel$export($4b922c411b6b6fb4$exports, "Button", () => $4b922c411b6b6fb4$expo
 
 
 function $4b922c411b6b6fb4$export$353f5b6fc5456de1({ children: children , variant: variant = "accent" , size: size = "normal" , outline: outline , onClick: onClick  }) {
-    return /*#__PURE__*/ (0, $aCh37$jsxDEV)("button", {
+    return /*#__PURE__*/ (0, $aCh37$jsx)("button", {
         className: (0, $aCh37$classnames)("daisy-btn", {
             "daisy-btn-sm": size === "small",
             "daisy-btn-outline": outline,
@@ -42,11 +42,7 @@ function $4b922c411b6b6fb4$export$353f5b6fc5456de1({ children: children , varian
         }),
         onClick: onClick,
         children: children
-    }, void 0, false, {
-        fileName: "packages/component-library/src/components/button/Button.tsx",
-        lineNumber: 26,
-        columnNumber: 5
-    }, this);
+    });
 }
 
 
@@ -55,20 +51,12 @@ var $ac5eabe48ee37674$exports = {};
 $parcel$export($ac5eabe48ee37674$exports, "Toast", () => $ac5eabe48ee37674$export$8d8dc7d5f743331b);
 
 function $ac5eabe48ee37674$export$8d8dc7d5f743331b({ message: message  }) {
-    return /*#__PURE__*/ (0, $aCh37$jsxDEV)("div", {
+    return /*#__PURE__*/ (0, $aCh37$jsx)("div", {
         className: "daisy-alert max-w-md justify-center shadow-lg",
-        children: /*#__PURE__*/ (0, $aCh37$jsxDEV)("span", {
+        children: /*#__PURE__*/ (0, $aCh37$jsx)("span", {
             children: message
-        }, void 0, false, {
-            fileName: "packages/component-library/src/components/toast/Toast.tsx",
-            lineNumber: 10,
-            columnNumber: 7
-        }, this)
-    }, void 0, false, {
-        fileName: "packages/component-library/src/components/toast/Toast.tsx",
-        lineNumber: 9,
-        columnNumber: 5
-    }, this);
+        })
+    });
 }
 
 
@@ -77,14 +65,10 @@ var $fa498bc7bee5d7e7$exports = {};
 $parcel$export($fa498bc7bee5d7e7$exports, "Navbar", () => $fa498bc7bee5d7e7$export$42cfbb80f7861e77);
 
 function $fa498bc7bee5d7e7$export$42cfbb80f7861e77({ children: children  }) {
-    return /*#__PURE__*/ (0, $aCh37$jsxDEV)("div", {
+    return /*#__PURE__*/ (0, $aCh37$jsx)("div", {
         className: "daisy-navbar bg-base-100",
         children: children
-    }, void 0, false, {
-        fileName: "packages/component-library/src/components/navbar/Navbar.tsx",
-        lineNumber: 8,
-        columnNumber: 10
-    }, this);
+    });
 }
 
 
@@ -94,42 +78,30 @@ $parcel$export($24b747ab2140c055$exports, "Card", () => $24b747ab2140c055$export
 $parcel$export($24b747ab2140c055$exports, "CardTitle", () => $24b747ab2140c055$export$474db65c3c394e1c);
 
 function $24b747ab2140c055$export$60332b2344f7fe41({ children: children  }) {
-    return /*#__PURE__*/ (0, $aCh37$jsxDEV)("div", {
+    return /*#__PURE__*/ (0, $aCh37$jsx)("div", {
         className: "daisy-card w-96 bg-neutral",
-        children: /*#__PURE__*/ (0, $aCh37$jsxDEV)("div", {
+        children: /*#__PURE__*/ (0, $aCh37$jsx)("div", {
             className: "daisy-card-body",
             children: children
-        }, void 0, false, {
-            fileName: "packages/component-library/src/components/card/Card.tsx",
-            lineNumber: 10,
-            columnNumber: 7
-        }, this)
-    }, void 0, false, {
-        fileName: "packages/component-library/src/components/card/Card.tsx",
-        lineNumber: 9,
-        columnNumber: 5
-    }, this);
+        })
+    });
 }
 function $24b747ab2140c055$export$474db65c3c394e1c({ title: title , action: action  }) {
-    return /*#__PURE__*/ (0, $aCh37$jsxDEV)("h2", {
+    return /*#__PURE__*/ (0, $aCh37$jsxs)("h2", {
         className: "daisy-card-title justify-between",
         children: [
             title,
             action
         ]
-    }, void 0, true, {
-        fileName: "packages/component-library/src/components/card/Card.tsx",
-        lineNumber: 23,
-        columnNumber: 5
-    }, this);
+    });
 }
 
 
+$parcel$exportWildcard($7c4abec25e5caac1$exports, $fc442c0e59a07833$exports);
 $parcel$exportWildcard($7c4abec25e5caac1$exports, $4b922c411b6b6fb4$exports);
 $parcel$exportWildcard($7c4abec25e5caac1$exports, $ac5eabe48ee37674$exports);
 $parcel$exportWildcard($7c4abec25e5caac1$exports, $fa498bc7bee5d7e7$exports);
 $parcel$exportWildcard($7c4abec25e5caac1$exports, $24b747ab2140c055$exports);
-$parcel$exportWildcard($7c4abec25e5caac1$exports, $fc442c0e59a07833$exports);
 
 
 
