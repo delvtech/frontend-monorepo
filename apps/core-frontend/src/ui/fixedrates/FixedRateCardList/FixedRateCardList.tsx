c7fd8b023931e7ee233b9151839938331c933154
Danny Delott <dannydelott@gmail.com>
2022-09-21 10:47:01 -0700

    Add optional bannerText prop to FixedRateCardWithZap

--- apps/core-frontend/src/ui/fixedrates/FixedRateCardList/FixedRateCardList.tsx
+++ apps/core-frontend/src/ui/fixedrates/FixedRateCardList/FixedRateCardList.tsx
@@ -1,13 +1,23 @@
 import { PrincipalTokenInfo } from "@elementfi/core-tokenlist";
-import tw from "efi-tailwindcss-classnames";
+import { aztecTerms } from "integrations/aztec/aztecAddresses";
 import { ReactElement } from "react";
+import { jt } from "ttag";
 import { useIsTailwindLargeScreen } from "ui/base/mediaBreakpoints";
 import { FixedRateCardWithZap } from "ui/fixedrates/FixedRateCard/FixedRateCardWithZap";
+
+import tw from "efi-tailwindcss-classnames";
+
 import { FixedRateCardListHeader } from "./FixedRateCardListHeader";
 
 interface FixedRateCardListProps {
   principalTokens: PrincipalTokenInfo[];
 }
+
+const aztecLink = (
+  <a href="https://zk.money/earn" key="aztec-link">
+    zk.money
+  </a>
+);
 
 export function FixedRateCardList(props: FixedRateCardListProps): ReactElement {
   const { principalTokens } = props;
@@ -25,6 +35,11 @@ export function FixedRateCardList(props: FixedRateCardListProps): ReactElement {
             <FixedRateCardWithZap
               key={principalToken.address}
               principalToken={principalToken}
+              bannerText={
+                aztecTerms.includes(principalToken.address)
+                  ? jt`✨ Want Privacy? Use Element via Aztec's ${aztecLink} for up to 30x cheaper, private transactions! ✨`
+                  : null
+              }
             />
           ))}
         </>
