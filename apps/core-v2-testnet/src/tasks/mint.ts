4b8938a3ae9610d776620bae459d3c6d374d7424
Cash DeLeon <cashd@users.noreply.github.com>
2022-09-22 01:47:53 -0500

    mint token task

--- apps/core-v2-testnet/src/tasks/mint.ts
+++ apps/core-v2-testnet/src/tasks/mint.ts
@@ -1,41 +1,44 @@
-import {
-  ERC20,
-  ERC20__factory,
-  MockERC20__factory,
-} from "@elementfi/core-v2-typechain";
+import { MockERC20__factory } from "@elementfi/core-v2-typechain";
 import { JsonRpcProvider } from "@ethersproject/providers";
-import { BigNumberish, ethers, Wallet, BigNumber } from "ethers";
-import { ethers as hEthers } from "hardhat";
+import { ethers } from "ethers";
+import { toBn } from "evm-bn";
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { giveTokens } from "src/helpers/giveTokens";
-
-interface MintTaskArgs {
-  tokenAddress: string;
-  account: string;
-  amount: string;
-}
 
 const { PRIVATE_KEY } = process.env;
+
+interface MintTaskArgs {
+  token: string;
+  who: string;
+  amount: string;
+}
 
 async function mintTokens(args: MintTaskArgs, provider: JsonRpcProvider) {
   if (!PRIVATE_KEY) {
     throw new Error("Error: no private key set in .env");
   }
+
   const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
-
-  const token = MockERC20__factory.connect(args.tokenAddress, provider);
-
+  const token = MockERC20__factory.connect(args.token, provider);
   const decimals = await token.decimals();
-  const modifer = 10 ^ decimals;
-  await token
+
+  console.log("Submitting transaction...");
+  const tx = await token
     .connect(wallet)
-    .mint(args.account, BigNumber.from(args.amount).mul(modifer));
+    .mint(args.who, toBn(args.amount, decimals));
+
+  console.log("Waiting for transaction...");
+  const reciept = await tx.wait();
+  console.log("Transaction hash: ", reciept.transactionHash);
 }
 
 export default task("mint", "mints tokens to an account")
   .addParam("token", "ERC20 token address")
-  .addParam("account", "account to recieve tokens")
-  .setAction(async (taskArgs: MintTaskArgs) =>
-    mintTokens(taskArgs, hEthers.provider),
+  .addParam("who", "account to recieve tokens")
+  .addParam(
+    "amount",
+    "amount of tokens to mint account, decimals applied automatically",
+  )
+  .setAction(async (taskArgs: MintTaskArgs, hre: HardhatRuntimeEnvironment) =>
+    mintTokens(taskArgs, hre.ethers.provider),
   );
