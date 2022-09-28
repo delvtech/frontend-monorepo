451370e40329cd0c095bab4c67800a627b3c544e
Ryan Goree <goree.ryan@gmail.com>
2022-09-28 15:30:04 -0500

    Fix graphql import names in core-v2-frontend

--- apps/core-v2-frontend/src/apolloClient.tsx
+++ apps/core-v2-frontend/src/apolloClient.tsx
@@ -1,15 +1,15 @@
 import { provider } from "src/provider";
-import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { getApolloLink } from "@elementfi/graphql";
-import { coreV2Graph } from "@elementfi/core-v2-graphql";
+import { elementGraph } from "@elementfi/core-v2-graphql";
 
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
   link: getApolloLink({
-    graphs: [coreV2Graph],
+    graphs: [elementGraph],
     provider: provider({
       // TODO: Clean this up
       chainId: parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || "5"),
     }),
-  }) as unknown as ApolloLink, // TODO: Find a better solution
+  }), // TODO: Find a better solution
 });
