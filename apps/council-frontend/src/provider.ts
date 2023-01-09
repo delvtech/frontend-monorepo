2420f4e671ca58b7290a990b78ec2bf02bcea5c2
Danny Delott <dannydelott@gmail.com>
2023-01-09 15:22:14 -0800

    Use the correct env variable (#432)

--- apps/council-frontend/src/provider.ts
+++ apps/council-frontend/src/provider.ts
@@ -16,8 +16,8 @@ export const { chains, provider } = configureChains(
   [
     // If a provider does not support a chain, it will fall back onto the next one
     // in the array.
-    alchemyProvider({ alchemyId: process.env.NEXT_PUBLIC_MAINNET_ALCHEMY_ID }),
-    alchemyProvider({ alchemyId: process.env.NEXT_PUBLIC_GOERLI_ALCHEMY_ID }),
+    alchemyProvider({ alchemyId: process.env.NEXT_PUBLIC_MAINNET_ALCHEMY_KEY }),
+    alchemyProvider({ alchemyId: process.env.NEXT_PUBLIC_GOERLI_ALCHEMY_KEY }),
     jsonRpcProvider({
       rpc: () => ({
         http: LOCAL_RPC_HOST,
