5bb4f67c68acbf4de56d9e5df816caa470821431
Danny Delott <dannydelott@gmail.com>
2023-01-09 18:44:36 -0800

    Remove broken gas widget (#433)

--- apps/council-frontend/src/ui/app/Header.tsx
+++ apps/council-frontend/src/ui/app/Header.tsx
@@ -1,8 +1,6 @@
 import React, { ReactElement } from "react";
 import { t } from "ttag";
-import { useGasPrice } from "src/ui/ethereum/useGasPrice";
 import ElementUrl from "src/urls";
-import GasIcon from "src/ui/base/svg/GasIcon";
 import { useDeposited } from "src/ui/base/lockingVault/useDeposited";
 import {
   ElementIconCircle,
@@ -14,11 +12,8 @@ import ExternalLink from "src/ui/base/ExternalLink/ExternalLink";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
 
-const GAS_URL = "https://www.etherchain.org/tools/gasnow";
-
 function Header(): ReactElement {
   const { address } = useAccount();
-  const { data: gasPrice } = useGasPrice();
 
   const amountDeposited = useDeposited(address) || "0";
   const formattedAmountDeposited = parseFloat(amountDeposited).toFixed(2);
@@ -35,17 +30,6 @@ function Header(): ReactElement {
                 className="text-principalRoyalBlue"
               />
             </span>
-
-            <ExternalLink
-              href={GAS_URL}
-              className="mr-8 flex items-center text-principalRoyalBlue"
-              showIcon={false}
-            >
-              <GasIcon className="h-5 w-5" />
-              <span className="font-bold">
-                {gasPrice?.recommendedBaseFee || 0.0}
-              </span>
-            </ExternalLink>
 
             <Tooltip content={t`${TooltipDefinition.OWNED_ELFI}`}>
               <span className="mr-8 flex items-center gap-2 font-bold text-principalRoyalBlue">
