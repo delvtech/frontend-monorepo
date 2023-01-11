e84d09a07ba9ed2fddca99154b7a0e0aff345250
Danny Delott <dannydelott@gmail.com>
2023-01-11 14:26:48 -0800

    Update TOS Banner (#436)

--- apps/core-frontend/src/ui/app/Banner/TermsBanner.tsx
+++ apps/core-frontend/src/ui/app/Banner/TermsBanner.tsx
@@ -1,7 +1,9 @@
 import React, { ReactElement, useEffect, useState } from "react";
-import { ElementMinLogo } from "ui/base/ElementMinLogo";
-import { jt, t } from "ttag";
-import { useIsTailwindSmallScreen } from "ui/base/mediaBreakpoints";
+import { t } from "ttag";
+import { AnchorButton, Button, Dialog, Intent } from "@blueprintjs/core";
+import { useDarkMode } from "ui/prefs/useDarkMode/useDarkMode";
+import classNames from "classnames";
+import { IconNames } from "@blueprintjs/icons";
 
 export const TOS_LOCAL_KEY = "Element-TOS-Acceptance";
 
@@ -16,15 +18,9 @@ const setAcceptedTOS = () => {
 export const TOS_URL =
   "https://elementfi.s3.us-east-2.amazonaws.com/element-finance-terms-of-service.pdf";
 
-const elementFinanceDiscordSupportChannelLink = (
-  <a
-    key="discord-support-channel-link"
-    href="https://discord.gg/tmn6h3XFV8"
-  >{t`here`}</a>
-);
 export const TermsBanner = (): ReactElement => {
   const [open, setOpen] = useState(false);
-  const isSm = useIsTailwindSmallScreen();
+  const { darkModeClassName } = useDarkMode();
   useEffect(() => {
     if (typeof window !== "undefined") {
       setOpen(!isTermsAccepted());
@@ -36,62 +32,57 @@ export const TermsBanner = (): ReactElement => {
   }
 
   return (
-    <div
-      className="fixed bottom-0 z-10 mb-48 h-24 w-full"
-      style={{
-        // tailwind breakpoints in current version stop at 256px (mb-64) :(
-        // this is a quick hackjob
-        marginBottom: isSm ? 400 : 320,
-      }}
+    <Dialog
+      className={classNames(darkModeClassName, "space-y-4")}
+      isOpen
+      isCloseButtonShown={false}
+      canEscapeKeyClose={false}
+      canOutsideClickClose={false}
+      title={t`Element.fi Terms and Conditions`}
     >
-      <div
-        className="sm:min-w-min m-auto mx-4 mb-4 flex max-w-3xl flex-col items-center justify-center 
-      rounded-lg border-2 border-white bg-gray-700 p-2 sm:m-auto sm:flex-row sm:p-10"
-      >
-        <ElementMinLogo
-          className="mt-6 -ml-12 hidden sm:block"
-          height={110}
-          width={110}
-        />
-        <div className="mr-4 max-w-lg text-xs">
-          <p>
-            {t`Element Finance, Inc. is committed to building an open, compliant, responsible system. 
-          We believe in building products for a healthy ecosystem. Although we don’t control the 
-          Protocol, which is open source, this UI employs a series of compliance filters and
-           internal controls. Element Finance maintains no control or custody of user funds.`}
-          </p>
+      <div className="space-y-4 px-4">
+        <p>
+          {t`Element Finance, Inc. is committed to building an open, compliant,
+          responsible system. We believe in building products for a healthy
+          ecosystem. Although we don't control the Protocol, which is open
+          source, this UI employs a series of compliance filters and internal
+          controls. Element Finance maintains no control or custody of user
+          funds.`}
+        </p>
+        <p>
+          {t`By clicking "Certify and Accept Terms", you represent and warrant that you (and any person on behalf of whom you are using Element’s platform/services) are:`}
+        </p>
+        <ol className="list-inside list-decimal space-y-4">
+          <li>{t`NOT identified on any list of sanctioned parties such as, for example, the lists maintained by the United Nations Security Council, the U.S. government (including the U.S. Treasury Department's Specially Designated Nationals (SDN) list, Foreign Sanctions Evaders list, the Sectoral Sanctions Identification List, the Non-SDN Menu Based Sanctions List), the European Union (EU) or its member states, and the government of your or such person’s home country if you or any such person is located outside the U.S. and EU;`}</li>
+          <li>{t`NOT located, ordinarily resident, organized, established, or domiciled in Cuba, Iran, North Korea, Syria, the Crimea region (including Sevastopol), or the self-proclaimed Donetsk People’s Republic or Luhansk People’s Republic regions;`}</li>
+          <li>{t`NOT owned or controlled by, or acting on behalf of, a person, an entity or the government of a country/territory identified in (2) or (3) above or the Government of Venezuela (including any of its agencies, instrumentalities or entities owned or controlled by it); and`}</li>
+          <li>{t`NOT directly or indirectly using any funds derived from, or otherwise representing the proceeds of, any illegal activities, to conduct activities on Element’s platform or use Element’s services.`}</li>
+        </ol>
+        <p>{t`Continued use of this service constitutes acceptance of our Terms of Service
+       and Privacy Policy.`}</p>
+      </div>
 
-          <p>{t`Because of regulatory events and guidance, some wallets and users may be permanently
-           or temporarily restricted from use of this UI, consistent with our Terms of Service and
-            relevant rules and laws. In some instances, the restriction will depend upon further
-             review and analysis, which we will attempt to resolve as quickly as possible. 
-             We've worked with our service providers to ensure that users who should not be 
-             restricted are free to use our UI.`}</p>
-          <p>{jt`If you think that you have been improperly restricted, please contact our 
-          Discord Support channel ${elementFinanceDiscordSupportChannelLink}.`}</p>
-          <p>{t`Continued use of this service constitutes acceptance of our Terms of Service
-           and Privacy Policy.`}</p>
-        </div>
-        <button
+      <div className="flex justify-center gap-4">
+        <Button
+          intent={Intent.PRIMARY}
           onClick={() => {
             setAcceptedTOS();
             setOpen(false);
           }}
-          className="whitespace-no-wrap my-2 cursor-pointer rounded-3xl bg-blue-500 p-3 text-xs font-bold text-white hover:bg-blue-600 sm:my-0 sm:mr-4"
         >
-          {t`Accept Terms`}
-        </button>
-        <a
+          {t`Certify and Accept Terms`}
+        </Button>
+
+        <AnchorButton
           target="_blank"
+          icon={IconNames.SHARE}
           rel="noreferrer"
           href={TOS_URL}
           className="hover:no-underline"
         >
-          <div className="whitespace-no-wrap cursor-pointer rounded-3xl bg-white p-3 text-xs font-bold text-blue-500 hover:bg-gray-300">
-            {t`Learn More`}
-          </div>
-        </a>
+          {t`Terms of Service`}
+        </AnchorButton>
       </div>
-    </div>
+    </Dialog>
   );
 };
