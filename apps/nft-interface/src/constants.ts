aff1ab07e2d0d46a72836d7c1c2f35bf821257f3
Ryan Goree <goree.ryan@gmail.com>
2024-10-18 00:40:17 -0500

    Bump ToS local storage key

--- apps/nft-interface/src/constants.ts
+++ apps/nft-interface/src/constants.ts
@@ -2,4 +2,4 @@ import moment from "moment";
 
 export const releaseDate = moment("6 Apr 22 18:00 EST"); // April 6th, 6:00 pm EST
 export const firstBatchMintMax = 4700;
-export const TOS_LOCAL_KEY = "Elfiverse-TOS-Acceptance-1719988498742";
+export const TOS_LOCAL_KEY = "Elfiverse-TOS-Acceptance-1729229926214";
